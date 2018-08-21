8df4ff8e21462d002ff4badd93fbeb808070ea28
echo <964126936@qq.com>
2018-08-22 06:48:13 +0800

    31

---
+++ day31_33/js/table.js
@@ -0,0 +1,110 @@
+function drawTable(){
+    var value = getRP()
+    var rValue = value[0]
+    var pValue = value[1]
+    var clearSD = selectValue(rValue, pValue)
+    console.log(clearSD)
+    //将clearSD变成数组
+    var sdArray = []
+    clearSD.forEach(function(x,j){
+        sdArray[j] = [x.product , x.region]
+        for(let i = 0 ; i<x.sale.length; i++){
+            sdArray[j].push(x.sale[i])
+        }
+    })
+    //画图
+    showTable(sdArray,rValue,pValue)
+}
+
+//获得选中的region和product
+function getRP(){
+    var regionsOne = []
+    var productsOne = []
+    regions.forEach(function(x){
+            if(x.checked == true){
+                regionsOne.push(x.parentElement.innerText)
+            }
+        })
+    products.forEach(function(x){
+        if(x.checked == true){
+            productsOne.push(x.parentElement.innerText)
+        }
+    }) 
+    return [regionsOne,productsOne]   
+}
+//获得清洗过后的sourceData的值
+function selectValue(rValue, pValue){
+    var rpValue = rValue +","+ pValue
+    
+    var clearSD = sourceData.filter(function(x){
+        return rpValue.indexOf(x.region) > -1 && rpValue.indexOf(x.product)>-1
+    })
+    return clearSD
+}
+
+function showTable(sdArray,rValue,pValue){
+    var table = document.createElement("table")
+    var tr = document.createElement("tr")
+    //var thArray = ["商品","地区","1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"]
+    //地区在前
+    if(rValue.length == 1 && pValue.length > 1){
+        //画表头
+        thArray.forEach(function(x,i){
+            var th = document.createElement("th")
+            if(i>1){
+                th.appendChild(document.createTextNode(x))
+                
+            }else if(i ==0 ){
+                th.innerText = thArray[1]
+            }else{
+                th.innerHTML = thArray[0]
+            }
+            tr.appendChild(th)
+        })
+        table.appendChild(tr)
+
+
+        //将获得数据商品列和地区列交换
+        sdArray.forEach(function(x){
+            temp = x[0]
+                x[0] = x[1]
+                x[1] = temp
+            })
+        //数据展示
+        tableValue(sdArray,pValue,table)
+
+    }else{
+        //表头
+        thArray.forEach(function(x){
+            var th = document.createElement("th")
+            th.appendChild(document.createTextNode(x))
+            tr.appendChild(th)
+        })
+        table.appendChild(tr)
+        //数据展示
+        tableValue(sdArray,rValue,table)   
+     }
+}
+
+function tableValue(sdArray,rpValue,table){
+    var tr;
+    var td;
+    sdArray.forEach(function(x,i){
+            tr = document.createElement("tr")
+            td = document.createElement("td")
+            if(i % rpValue.length == 0){
+                td.innerText = x[0]
+                td.setAttribute("rowspan",rpValue.length)
+                tr.appendChild(td)
+            }else{
+
+            }
+            for(let j = 1; j<sdArray[i].length; j++){
+                td = document.createElement("td")
+                td.innerText = x[j]
+                tr.appendChild(td)
+            }
+            table.appendChild(tr)
+    })
+    tableWrapper.appendChild(table)
+}
---
+++ day34_36/js/bar.js
@@ -0,0 +1,61 @@
+//获得数据
+function getValue(){
+   // 固定只显示华东地区手机12个月的数据
+   var eastPhone = sourceData.filter(function(x){
+       return x.region == "华东" && x.product == "手机"
+   }) 
+   var sale = eastPhone[0].sale;
+   bar(sale)
+}
+
+function bar(sale) {
+    // 定义好柱状图绘制区域的高度，宽度，轴的高度，宽度
+    // 定义好每一个柱子的宽度及柱子的间隔宽度
+    // 定义好柱子颜色，轴的颜色
+    var startX = 50
+    var startY = 400
+    var width = 450
+    var height = 350
+    var axisWidth = 400
+    var axisHeight = 300
+    var barWidth = 20
+    var space = 10
+    var barColor = "blue"
+    var axisColor = "#fff"
+
+    var svgStart ='<svg width="600" height="600" version="1.1" xmlns="http://www.w3.org/2000/svg">'
+    var svgEnd = '</svg>'
+
+    // 拿到柱状图中的最大值Max
+    // 根据Max和你用来绘制柱状图图像区域的高度，进行一个数据和像素的折算比例
+    console.log(sale instanceof Array)
+    var barHMax = 0
+    
+    sale.forEach(function(x){
+        barHMax = x>barHMax ? x:barHMax
+    })
+    
+
+    var barHMax 
+    var barHArray
+    var charts = document.getElementById("t")
+    // 绘制横轴及纵轴
+    var row = "<line x1="+ startX + " y1=" + startY + " x2=" + 450 + " y2=" + 400 + ' style="stroke:rgb(0,99,99);stroke-width:2"/>'
+    var col = "<line x1="+ startX + " y1=" + startY + " x2=" + startX + " y2=" + 100 + ' style="stroke:rgb(0,99,99);stroke-width:2"/>'
+
+    var svgT = svgStart + row + col
+    
+    console.log(charts.innerHTML)
+    for(let i = 0; i < sale.length;i++){
+        
+        let rectStartX = startX + space * (i+1) + barWidth * i
+        let rectStartY = startY - sale[i]
+        var bar = "<rect x=" + rectStartX +" y=" +rectStartY + " width=" + barWidth + " height=" + sale[i] +' style="fill:rgb(0,0,255);stroke-width:1;stroke:rgb(0,0,0)"/>'
+        svgT += bar
+    }
+    svgT += svgEnd
+
+    charts.innerHTML = svgT 
+}
+
+getValue()
