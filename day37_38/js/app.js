83168e584abaa15930d142df71395d22901a3d11
echo <964126936@qq.com>
2018-08-22 22:26:19 +0800

    37

---
+++ day37_38/js/app.js
@@ -0,0 +1,5 @@
+var tableWrapper = document.getElementById("table-wrapper")
+//表头
+var thArray = ["商品","地区","1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"]
+var regions = document.querySelectorAll("#" + "region-radio-wrapper" + " input[checkbox-type=one] ")
+var products = document.querySelectorAll("#" + "product-radio-wrapper" + " input[checkbox-type=one] ")
